71402b00345b19a7a02cf2514d946814e85b5b8b
Crescent Sambila <crescent.sambila@gmail.com>
2025-10-29 13:41:48 +0300

    Created zones repositories for all maps operations

---
+++ lib/features/land_use/zoning/domain/repositories/zoning_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:latlong2/latlong.dart';
+import '../../../../../core/error/failures.dart';
+import '../entities/basemap.dart';
+import '../entities/user_location.dart';
+import '../entities/zoning_feature.dart';
+
+abstract class ZoningRepository {
+  // Basemap operations
+  Future<Either<Failure, bool>> isBasemapDownloaded(String localityId);
+  Future<Either<Failure, Basemap>> loadBasemap(String localityId);
+  Future<Either<Failure, bool>> downloadBasemap({
+    required String localityId,
+    String? accessToken,
+    Function(double)? onProgress,
+  });
+  Future<Either<Failure, bool>> deleteBasemap(String localityId);
+
+  // Location operations
+  Future<Either<Failure, bool>> checkLocationPermissions();
+  Future<Either<Failure, UserLocation>> getCurrentLocation({
+    List<LatLng>? projectBoundary,
+  });
+  Stream<UserLocation> getLocationStream({List<LatLng>? projectBoundary});
+
+  // Feature operations
+  Future<Either<Failure, List<ZoningFeature>>> getFeatures(String projectId);
+  Future<Either<Failure, ZoningFeature>> createFeature(ZoningFeature feature);
+  Future<Either<Failure, ZoningFeature>> updateFeature(ZoningFeature feature);
+  Future<Either<Failure, bool>> deleteFeature(String featureId);
+  
+  // Export operations
+  Future<Either<Failure, bool>> exportFeatures(String projectId, String filePath);
+  Future<Either<Failure, List<ZoningFeature>>> getUnsyncedFeatures(String projectId);
+}
