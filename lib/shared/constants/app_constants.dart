4633e604ec5e609ec27a7b7b5b33a763b4451f5f
Crescent Sambila <crescent.sambila@gmail.com>
2025-11-08 02:04:45 +0300

    Updated colors

--- lib/shared/constants/app_constants.dart
+++ lib/shared/constants/app_constants.dart
@@ -1,4 +1,19 @@
+import 'package:flutter/material.dart';
+
 class AppConstants {
+  
+  // Colors (Light Theme)
+  static const Color primaryColor = Color(0xFF2196F3);
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color borderColor = Color(0xFFE0E0E0);
+
+  // Colors (Dark Theme)
+  static const Color darkPrimary = Color(0xFF64B5F6);
+  static const Color darkTextPrimary = Color(0xFFFFFFFF);
+  static const Color darkTextSecondary = Color(0xFFB0B0B0);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkBorder = Color(0xFF424242);
   
   // Spacing
   static const double spacingXs = 4.0;
